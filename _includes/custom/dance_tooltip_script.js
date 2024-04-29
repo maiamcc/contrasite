264d113526575058f506e484322b70064de04f4f
Harris Lapiroff <harris@freedom.press>
2024-04-29 13:01:18 -0400

    Add card icons to links to card tooltips

--- _includes/custom/dance_tooltip_script.js
+++ _includes/custom/dance_tooltip_script.js
@@ -1,9 +1,15 @@
 <script>
-    document.addEventListener('DOMContentLoaded', function () {
+    {% if page.dances %}
+    document.addEventListener('DOMContentLoaded', async function () {
+        let currentLink;
+        let iconCode = (await (await fetch('/assets/images/icon-dance-tip.svg')).text()).replace(/<\?[^\?]+\?>/g, '').replace("\n", '');
         // NB: dance data tag must match that of the <a> element, so make sure
         // this code and custom/dance_tooltip_link.html use the same slugify func
         {% for dance in page.dances %}
-        tippy('[data-dance-name="{{dance.title | slugify}}"]', {
+        currentLink = document.querySelector('[data-dance-name="{{dance.title | slugify}}"]');
+        currentLink.innerHTML += iconCode;
+        currentLink.querySelector('svg').classList.add('icon');
+        tippy(currentLink, {
             content: `{% include custom/dance.html %}`,
             allowHTML: true,
             trigger: 'click',
@@ -13,4 +19,5 @@
         });
         {% endfor %}
     });
+    {% endif %}
 </script>
